44f4a0ee38e57be387ccbfeec15df8a33889c46f
Christine Contreras <christineec23@yahoo.com>
2021-06-29 17:55:30 -0500

    fsearch and form working

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -6,8 +6,10 @@ import { Container } from 'semantic-ui-react'
 
 class PokemonPage extends React.Component {
   state = {
-    pokemons: []
+    pokemons: [],
+    search: ""
   }
+
   componentDidMount() {
     fetch('http://localhost:3000/pokemon')
     .then(response => response.json())
@@ -15,16 +17,52 @@ class PokemonPage extends React.Component {
       this.setState({pokemons: json})
     })
   }
+
+  handleInputChange = (event) => this.setState({search: event.target.value})
+
+  addPokemon = (info) => {
+    const formData = {
+      "name": info.name,
+      "hp": info.hp,
+      "sprites": {
+        "front": info.frontUrl,
+        "back": info.backUrl
+      }
+    }
+
+    const objectConfig = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    }
+
+    fetch('http://localhost:3000/pokemon', objectConfig)
+    .then(response => response.json())
+    .then(json => {
+      this.setState(previousState => {
+        return {
+          pokemons: [...previousState.pokemons, json]
+        }
+      })
+    })
+  }
+  
   render() {
+
+    const filteredPokemon = this.state.pokemons.filter(pokemon => pokemon.name.includes(this.state.search))
+
     return (
       <Container>
         <h1>Pokemon Searcher</h1>
         <br />
-        <PokemonForm />
+        <PokemonForm addPokemon={this.addPokemon}/>
         <br />
-        <Search />
+        <Search handleInputChange={this.handleInputChange}/>
         <br />
-        <PokemonCollection pokemons={this.state.pokemons}/>
+        <PokemonCollection pokemons={filteredPokemon}/>
       </Container>
     )
   }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Search = props => {
-  return (
-    <div className="ui search">
+export class Search extends Component {
+  render() {
+    return (
+      <div className="ui search">
       <div className="ui icon input">
-        <input className="prompt"/>
+        <input className="prompt" type="text" name="search" placeholder="pickachu" onChange={this.props.handleInputChange}/>
         <i className="search icon" />
       </div>
     </div>
-  )
+    )
+  }
 }
 
 export default Search
